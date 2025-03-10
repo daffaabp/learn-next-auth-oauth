eec6bb448c4b1a6e3facf7579f9d44fd5b96cb4d
Daffa Budi Prasetya <daffa.budi2003@gmail.com>
2025-03-10 11:45:12 +0700

    Tambahkan logika autentikasi dasar menggunakan NextAuth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,14 @@
 import { SignOut } from "@/components/sign-out";
+import { auth } from "@/lib/auth";
+import { redirect } from "next/navigation";
 
 const Page = async () => {
+	const session = await auth();
+
+	if (!session) {
+		redirect("/sign-in");
+	}
+
 	return (
 		<>
 			<div className="bg-gray-100 rounded-lg p-4 text-center mb-6">
